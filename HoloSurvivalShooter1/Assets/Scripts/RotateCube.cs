2fe4f4c61100303f077e95277ec907c9785c4a1c
Harry Yoon <harry.yoon@opensoftdev.com>
2016-06-29 14:24:57 -0700

    Updated HoloLens app project template.

---
+++ HoloSurvivalShooter1/Assets/Scripts/RotateCube.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateCube : MonoBehaviour {
+
+    Transform cube;
+	void Start () {
+        cube = GetComponent<Transform>();
+	}
+	
+	void Update () {
+        cube.Rotate(0.1f, 0.2f, 0.3f);
+	}
+
+}
